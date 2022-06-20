70548ec1233e575692d373e340e9bdd0caa7fc64
Evgeny Zbirun <zbirunzbirun@gmail.com>
2022-06-19 22:03:01 -0700

    changed html and started js file.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// User Interface Logic
+
+
+
+let form = document.querySelector("form#ask");
+
+form.addEventListener("submit", function (event) {
+  let input1 = document.getElementById("q1")
+  let input2 = document.getElementById("q2")
+  let input3 = document.getElementById("q3")
+  let input4 = document.getElementById("q4")
+  let input5 = document.getElementById("q5")
+
+
+})
